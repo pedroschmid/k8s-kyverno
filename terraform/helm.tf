518d5b31d9d94b0174062acca3f4bdbf2ac8fe94
Pedro <pedro.schmid2311@gmail.com>
2025-07-03 02:34:12 -0300

    feat(terraform): provision Minikube cluster and deploy Kyverno via Helm

---
+++ terraform/helm.tf
@@ -0,0 +1,13 @@
+resource "helm_release" "kyverno" {
+  name       = var.helm_name
+  namespace  = var.helm_namespace
+  chart      = var.helm_chart
+  repository = var.helm_repository
+  version    = var.helm_version
+
+  create_namespace = true
+
+  depends_on = [
+    minikube_cluster.docker
+  ]
+}
