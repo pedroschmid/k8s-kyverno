63503d819560f9dafe7b0d1d6dadbc2a3f430b2a
Pedro <pedro.schmid2311@gmail.com>
2025-07-03 02:34:15 -0300

    feat(terraform): apply Kyverno ClusterPolicies using kubectl provider

---
+++ terraform/policies.tf
@@ -0,0 +1,17 @@
+locals {
+  policy_files = fileset("${path.module}/policies", "**/*.yaml")
+
+  policies = {
+    for file in local.policy_files : file => file("${path.module}/policies/${file}")
+  }
+}
+
+resource "kubectl_manifest" "policies" {
+  for_each  = local.policies
+  yaml_body = each.value
+
+  depends_on = [
+    minikube_cluster.docker,
+    helm_release.kyverno
+  ]
+}
