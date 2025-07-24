348631f1411d4170d262eb3fb6cf5f101fb3c45d
Pedro Abreu <47400938+pedroschmid@users.noreply.github.com>
2025-07-24 11:46:30 -0300

    chore: add variable description

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,35 +1,44 @@
 variable "cluster_name" {
-  type = string
+  type        = string
+  description = "The name of the Kubernetes cluster where Kyverno will be deployed."
 }
 
 variable "cluster_driver" {
-  type = string
+  type        = string
+  description = "The infrastructure or cloud provider driver used for the Kubernetes cluster (e.g., EKS, GKE, AKS, kind, minikube)."
 }
 
 variable "cluster_version" {
-  type = string
+  type        = string
+  description = "The version of the Kubernetes cluster to ensure compatibility with Kyverno."
 }
 
 variable "cluster_addons" {
-  type = list(string)
+  type        = list(string)
+  description = "A list of additional cluster add-ons or components to be deployed along with Kyverno (e.g., metrics-server, ingress controllers)."
 }
 
 variable "helm_name" {
-  type = string
+  type        = string
+  description = "The Helm release name for deploying the Kyverno chart."
 }
 
 variable "helm_namespace" {
-  type = string
+  type        = string
+  description = "The Kubernetes namespace where Kyverno will be installed using Helm."
 }
 
 variable "helm_repository" {
-  type = string
+  type        = string
+  description = "The URL of the Helm repository containing the Kyverno chart."
 }
 
 variable "helm_chart" {
-  type = string
+  type        = string
+  description = "The name of the Kyverno Helm chart to be installed."
 }
 
 variable "helm_version" {
-  type = string
+  type        = string
+  description = "The specific version of the Kyverno Helm chart to deploy."
 }
